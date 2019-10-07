b00fee3d7790547ad6eb87612d728399971329ff
Webmaster0104 <awesomewebdev01@gmail.com>
2019-10-08 05:09:03 +1000

    babel

--- next.config.js
+++ next.config.js
@@ -1,2 +1,47 @@
+const path = require('path')
+const glob = require('glob')
+
+module.exports = {
+    webpack: (config, {dev})=>{
+        config.module.rules.push(
+            {
+                test: /\.(css\scss)/,
+                loader: "emit-file-loader",
+                options: {
+                    name: "dist/[path][name].[ext]"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['babel-loader', 'raw-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.s(a|c)scc$/,
+                use: [
+                    "babel-loader",
+                    "raw-loader",
+                    "postcss-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            includePaths: ['scss','node_modules']
+                                .map(d => path.join(__dirname,d))
+                                .map(g => glob.sync(g))
+                                .reduce((a,c) => a.concat(c), [])
+                        }
+                    }
+                ]
+            }
+        )
+    }
+}
+
+
+
+
+
+
+
+
 const withSass = require('@zeit/next-sass')
 module.exports = withSass()
